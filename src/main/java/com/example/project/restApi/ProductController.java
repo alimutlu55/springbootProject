6bca48d3a3a1cfccd50f6ccc0ff9d945042f2812
almtl55 <alimutlu_5529@icloud.com>
2020-12-18 11:15:18 +0300

    düzenlememeler yapıldı

--- src/main/java/com/example/project/restApi/ProductController.java
+++ src/main/java/com/example/project/restApi/ProductController.java
@@ -28,7 +28,7 @@ public class ProductController {
          iProductService.addProduct(product);
     }
 
-    @PostMapping("/getById")
+    @GetMapping("/getById/{id}")
     public Product getProductById (@PathVariable int id) {
         return iProductService.getProductById(id);
     }
