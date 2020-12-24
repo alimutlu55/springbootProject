47576809ba66835087efb99063d19ed494197368
almtl55 <alimutlu_5529@icloud.com>
2020-12-24 15:25:59 +0300

    product objesine yeni alanlar eklendi

--- src/main/java/com/example/project/models/Product.java
+++ src/main/java/com/example/project/models/Product.java
@@ -3,6 +3,8 @@ package com.example.project.models;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Currency;
+
 
 @Document
 public class Product {
@@ -12,17 +14,56 @@ public class Product {
     private String productName;
     private String productCategory;
     private String productModel;
+    private int productCategoryId;
+    private Currency productPrice;
+    private String productImgUrl;
+    private String productDescription;
 
-
-    public Product(int productId, String productName, String productCategory, String productModel) {
+    public Product(int productId, String productName, String productCategory, String productModel, int productCategoryId, Currency productPrice, String productImgUrl, String productDescription) {
         this.productId = productId;
         this.productName = productName;
         this.productCategory = productCategory;
         this.productModel = productModel;
+        this.productCategoryId = productCategoryId;
+        this.productPrice = productPrice;
+        this.productImgUrl = productImgUrl;
+        this.productDescription = productDescription;
     }
 
     public Product() {
 
+    }
+
+    public int getProductCategoryId() {
+        return productCategoryId;
+    }
+
+    public void setProductCategoryId(int productCategoryId) {
+        this.productCategoryId = productCategoryId;
+    }
+
+    public Currency getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(Currency productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductImgUrl() {
+        return productImgUrl;
+    }
+
+    public void setProductImgUrl(String productImgUrl) {
+        this.productImgUrl = productImgUrl;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
     }
 
     public int getProductId() {
